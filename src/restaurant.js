dc4edd746161d80523fffd4cc01eb32b7349e254
Chris <ffson1999@yahoo.com>
2024-06-03 06:32:09 -0400

    - Added ID to nav buttons in index.html - Created menu.js and contact.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -5,18 +5,18 @@ const createRestaurantHomePage = () => {
   const pageContent = document.createElement('div');
   pageContent.classList.add('page-content');
 
+  //Create and append the header element
+  const header = document.createElement('h1');
+  header.textContent = "Welcome to Shiloh's Seafood!";
+  header.classList.add('header');
+  pageContent.appendChild(header);
+
   //Create and appened the image element
   const image = document.createElement('img');
   image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Barbieri_-_ViaSophia25668.jpg/1920px-Barbieri_-_ViaSophia25668.jpg';
   image.classList.add('image');
   image.height = '300';
   pageContent.appendChild(image);
-
-  //Create and append the header element
-  const header = document.createElement('h1');
-  header.textContent = "Welcome to Shiloh's Seafood!";
-  header.classList.add('header');
-  pageContent.appendChild(header);
 
   //Create and append the paragraph element
   const paragraph1 = document.createElement('p');
